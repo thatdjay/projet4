8518e33e57fb03fc612f72a7ac23a7e1a56d465a
jeremie <201936979@cegepsherbrooke.qc.ca>
2021-12-02 13:32:37 -0500

    gpio

---
+++ rpi-gpio-wpi.c
@@ -0,0 +1,25 @@
+#include <wiringPi.h> //inclus la librairie wiringpi
+
+//librairies pour utiliser des strings
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char argv[])
+{
+    wiringPiSetupGpio(); //initialize la librairie
+    pinMode(atoi(argv[1]), OUTPUT); //met la broche gpio sélectionnée en sortie
+
+    if(argv[2] == "on") //si l'argument est on
+    {
+        printf("on"); //confirme en affichant "on" dans le terminal
+        digitalWrite(atoi(argv[1]), HIGH); //met la broche à 1
+    }
+    else if(argv[2] == "off") //si l'argument est off
+    {
+        printf("off"); //affiche off
+        digitalWrite(atoi(argv[1]), LOW); //met la briche à 0
+    }else printf("Argument non valide") //ne fait rien si l'argument n'est pas valide
+
+    return 0;
+}
